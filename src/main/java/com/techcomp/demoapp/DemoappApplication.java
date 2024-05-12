4916cd4a51dadbda9b5caf005fa364eca8e0b825
RehmanrRaoo <abdurrehmanrao50@gmail.com>
2024-05-13 00:10:04 +0530

    addSOP

--- src/main/java/com/techcomp/demoapp/DemoappApplication.java
+++ src/main/java/com/techcomp/demoapp/DemoappApplication.java
@@ -8,6 +8,7 @@ public class DemoappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoappApplication.class, args);
+		System.out.println("done something");
 	}
 
 
